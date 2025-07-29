e54c620db6764887009bd7af2570a1fbb09220d6
gcanechia <gcanechia@provertec.com.br>
2025-07-29 17:10:48 -0300

    Nova entidade: Transação

---
+++ Domain/Models/Enum/TipoTransacao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Enum
+{
+    public enum TipoTransacao
+    {
+        Entrada = 1,
+        Saida = 2
+    }
+}
---
+++ Domain/Models/Transacao.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models.Enum;
+
+namespace Domain.Models
+{
+    public class Transacao : Entity
+    {
+        public TipoTransacao TipoTransacao { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataTransacao { get; set; }
+        public string Empresa { get; set; }
+    }
+}
